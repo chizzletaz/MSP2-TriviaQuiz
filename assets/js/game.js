ea66860ff40ed2be641d5a98d113f03186ddd320
chizzletaz <epitax@hotmail.com>
2021-03-03 09:32:24 +0000

    Add game.js script to challenge.html. Check for a response from the Open Trivia DB API.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
 const question = document.getElementById('question');
 const answers = Array.from(document.getElementsByClassName('answer-text')); 
 
+fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
+    .then(response =>{
+        console.log(response);
+    })
+
+
