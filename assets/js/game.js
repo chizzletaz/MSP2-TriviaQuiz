77e1c23a5e69168e53a1a496e4c655f04ab4d32a
chizzletaz <epitax@hotmail.com>
2021-03-03 09:42:44 +0000

    Add API-response to loadedQustions.

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,11 @@ const answers = Array.from(document.getElementsByClassName('answer-text'));
 
 fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
     .then(response =>{
-        console.log(response);
+        return response.json()
     })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions);
+    });
+  
 
 
