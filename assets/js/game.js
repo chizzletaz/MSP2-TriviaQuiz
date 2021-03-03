39c94e6cfd157e3202c57d556678e715addede02
chizzletaz <epitax@hotmail.com>
2021-03-03 11:47:23 +0000

    add data-number attrbute to challenge.html. Add startGame function to game.js.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,42 @@
 const question = document.getElementById('question');
 const answers = Array.from(document.getElementsByClassName('answer-text')); 
 
+
+
 let questions = [];         // an Array of questions and answers
 
+// get questions from API - credit: James Q Quick: https://www.youtube.com/watch?v=3aKOQn2NPFs
 fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
     .then(response => {
         return response.json()
     })                                                
-    .then(loadedQuestions => {                                        //credit: James Q Quick: https://www.youtube.com/watch?v=3aKOQn2NPFs
+    .then(loadedQuestions => {                                        
         questions = loadedQuestions.results.map(loadedQuestion => {   //format the data from the API to the format that we need, an Array of objects.
             const formattedQuestion  = {                     
                 question: loadedQuestion.question                     //question object
             };
             
-            const answersOption = [...loadedQuestion.incorrect_answers];    //get incorrect answers from API
-            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1; //make the index of the correct answer random
+            const answersOption = [...loadedQuestion.incorrect_answers];    //get the incorrect answers from API
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;   //make the index of the correct answer random
             answersOption.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);  // add the correct answer to the array of incorrect answers
             
-            answersOption.forEach((optiom, index) => {                       //add answers to formattedQuestions object
+            answersOption.forEach((option, index) => {                       //add answers to formattedQuestions object
                 formattedQuestion["option" + (index + 1)] = option;
             });
-            console.log(formattedQuestion);
-    });
+            
+            return formattedQuestion;
+    })
+    .catch(error => {
+        console.log(error);
 });
+
+function startGame() {
+    counter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+}
+
+function getNewQuestion() {
+    
+}
