7264c67ac766765153c135aa3925968070aeb706
chizzletaz <epitax@hotmail.com>
2021-03-03 10:58:38 +0000

    get incorrect answers from API.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,19 @@
 const question = document.getElementById('question');
 const answers = Array.from(document.getElementsByClassName('answer-text')); 
 
-let questions = [];
+let questions = [];         // an Array of questions and answers
 
 fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
     .then(response => {
         return response.json()
-    })
-    .then(loadedQuestions => {
-        questions = loadedQuestions.results.map(loadedQuestion => {    //format the info from API to the 
-            const formattedQuestion  = {                    // format that we need, an object.
-                question: loadedQuestion.question
+    })                                                //credit: James Q Quick: https://www.youtube.com/watch?v=3aKOQn2NPFs
+    .then(loadedQuestions => {                         
+        questions = loadedQuestions.results.map(loadedQuestion => {   //format the data from API to the format that we need, an Array of objects.
+            const formattedQuestion  = {                     
+                question: loadedQuestion.question                   //question object
             };
-        console.log(formattedQuestion)
+        
+            const answers = [...loadedQuestion.incorrect_answers];
+            console.log(answers);
     });
 });
