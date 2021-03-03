9af7a1deabd26f05c29ea151161f283838eaf287
chizzletaz <epitax@hotmail.com>
2021-03-03 10:31:52 +0000

    Get questions from API and format it into a question object.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,17 @@
 const question = document.getElementById('question');
 const answers = Array.from(document.getElementsByClassName('answer-text')); 
 
+let questions = [];
+
 fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
-    .then(response =>{
+    .then(response => {
         return response.json()
     })
     .then(loadedQuestions => {
-        console.log(loadedQuestions);
+        questions = loadedQuestions.results.map(loadedQuestion => {    //format the info from API to the 
+            const formattedQuestion  = {                    // format that we need, an object.
+                question: loadedQuestion.question
+            };
+        console.log(formattedQuestion)
     });
-  
-
-
+});
