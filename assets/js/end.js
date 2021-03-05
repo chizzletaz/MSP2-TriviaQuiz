20f450e8f247ecc1f4ce02696846f4fe224fbc6c
chizzletaz <epitax@hotmail.com>
2021-03-05 12:43:47 +0000

    Feat: remove disabled when user types.

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,11 @@
 const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
 
+
+//remove disables from Save-button when user types in name.
+//credit: James Q Quick: https://www.youtube.com/watch?v=o3MF_JmQxYg
 username.addEventListener('keyup', () => {
-    console.log(username.value);
+    saveScoreBtn.disabled = !username.nodeValue; //if nothing is typed in the inputbox, the saveScoreBtn is disabled.
 })
 
 function saveHighScore(event) {
