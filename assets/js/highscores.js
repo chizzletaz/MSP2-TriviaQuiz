2119d87ae67d370ec3f1f6d032e2231a88b404e8
chizzletaz <epitax@hotmail.com>
2021-03-06 14:05:13 +0000

    Feat: add highscores to highscores.html.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,6 +1,9 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];  //get highscores from localStorage.
 
-highScores.map(score => {
-    console.log(score);
-});
+
+// Add highscores to highscore.html. Credit: James Q Quick: https://www.youtube.com/watch?v=jfOv18lCMmw
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+})
+.join("");
