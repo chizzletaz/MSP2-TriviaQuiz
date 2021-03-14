ed388ee95266ed191127099b4cd395c9fdf421f2
chizzletaz <epitax@hotmail.com>
2021-03-14 14:02:08 +0000

    start with rewriting javascript used for challenge to practice.js.

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -3,116 +3,102 @@ $(document).ready(function() {
 });
 
 
-// const question = document.getElementById('question');
-// const answers = Array.from(document.getElementsByClassName('answer-text'));
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('answer-text'));
 
-// const questionCounterText = document.getElementById("questionCounter");
-// const scoreText = document.getElementById("score");
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
-// const progressBarFull = document.getElementById("progressBarFull");
-// const loader = document.getElementById("loader");
-// const game = document.getElementById("game");
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
-// const musicOn = document.getElementById("music-on");
-// const musicOff = document.getElementById("music-off");
-// const soundOn = document.getElementById('sound-on');
-// const soundOff = document.getElementById('sound-off');
+const musicOn = document.getElementById("music-on");
+const musicOff = document.getElementById("music-off");
+const soundOn = document.getElementById('sound-on');
+const soundOff = document.getElementById('sound-off');
 
-// const nextQuestion = document.getElementById("next-question");
-// const levelText = document.getElementById('level');
+const nextQuestion = document.getElementById("next-question");
+const levelText = document.getElementById('level');
 
-// let questions = [];         /* an Array of questions and answers */
-// let score = 0;
-// let questionCounter = 0;
-// let availableQuestions = [];
-// let currentQuestion = {};
+let questions = [];         /* an Array of questions and answers */
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let currentQuestion = {};
 
-// let level = 1;
+const category = document.getElementById('categories').value;
+const difficulty = document.getElementById('difficulties').value;
+console.log(difficulty);
 
-// const urlEasy = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
-// const urlMedium = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
-// const urlHard = "https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple";
 
-// window.onload = fetchQuestions(urlEasy);
+/* get questions from API - credit: James Q Quick: https://www.youtube.com/watch?v=3aKOQn2NPFs */
+function fetchQuestions(url) {
+    fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(loadedQuestions => {
+            questions = loadedQuestions.results.map(loadedQuestion => {   //format the data from the API to the format that we need, an Array of objects.
+                const formattedQuestion = {
+                    question: loadedQuestion.question                     //question object
+                };
 
-// /* get questions from API - credit: James Q Quick: https://www.youtube.com/watch?v=3aKOQn2NPFs */
-// function fetchQuestions(url) {
-//     fetch(url)
-//         .then(response => {
-//             return response.json()
-//         })
-//         .then(loadedQuestions => {
-//             questions = loadedQuestions.results.map(loadedQuestion => {   //format the data from the API to the format that we need, an Array of objects.
-//                 const formattedQuestion = {
-//                     question: loadedQuestion.question                     //question object
-//                 };
+                const answerOptions = [...loadedQuestion.incorrect_answers];    //get the incorrect answers from API
+                formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;   //make the index of the correct answer random
+                answerOptions.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);  // add the correct answer to the array of incorrect answers
 
-//                 const answerOptions = [...loadedQuestion.incorrect_answers];    //get the incorrect answers from API
-//                 formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;   //make the index of the correct answer random
-//                 answerOptions.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);  // add the correct answer to the array of incorrect answers
+                answerOptions.forEach((option, index) => {                       //add answers to formattedQuestions object
+                    formattedQuestion["option" + (index + 1)] = option;
+                });
 
-//                 answerOptions.forEach((option, index) => {                       //add answers to formattedQuestions object
-//                     formattedQuestion["option" + (index + 1)] = option;
-//                 });
+                return formattedQuestion;
+            });
+            startGame();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
 
-//                 return formattedQuestion;
-//             });
-//             switch (level) {
-//                 case 1:
-//                     startGame();
-//                     break;
-//                 case 2:
-//                     startGame2();
-//                     break;
-//                 case 3:
-//                     startGame3();
-//                     break;
-//             }
+// Constants 
+const correct_PointsL1 = 10;
+const correct_PointsL2 = 20;
+const correct_PointsL3 = 30;
 
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         });
-// };
+const max_Questions = 3;    //will be 10 when everything functions.
 
-// // Constants 
-// const correct_PointsL1 = 10;
-// const correct_PointsL2 = 20;
-// const correct_PointsL3 = 30;
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+    loader.classList.add('hidden');
+    game.classList.remove('hidden');
 
-// const max_Questions = 3;    //will be 10 when everything functions.
+};
 
-// function startGame() {
-//     questionCounter = 0;
-//     score = 0;
-//     availableQuestions = [...questions];
-//     getNewQuestion();
-//     loader.classList.add('hidden');
-//     game.classList.remove('hidden');
+function getNewQuestion() {
+    levelText.innerHTML = `Level: ${level}`;    /* update the level the user is playing */
+    questionCounter++;
+    questionCounterText.innerText = `Question: ${questionCounter}/${max_Questions}`;
+    // update the progress bar. credit James Q Quick: https://www.youtube.com/watch?v=4bctmtuZVcM
+    progressBarFull.style.width = `${(questionCounter / max_Questions) * 100}%`;
 
-// };
+    //get a random question from available questions
+    const questionIndex = Math.floor(availableQuestions.length * Math.random());
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerHTML = currentQuestion.question;
 
-// function getNewQuestion() {
-//     levelText.innerHTML = `Level: ${level}`;    /* update the level the user is playing */
-//     questionCounter++;
-//     questionCounterText.innerText = `Question: ${questionCounter}/${max_Questions}`;
-//     // update the progress bar. credit James Q Quick: https://www.youtube.com/watch?v=4bctmtuZVcM
-//     progressBarFull.style.width = `${(questionCounter / max_Questions) * 100}%`;
+    //Add answers to the challenge.html page
+    answers.forEach(answer => {
+        const number = answer.dataset.number;
+        answer.innerHTML = currentQuestion['option' + number]
+    });
 
-//     //get a random question from available questions
-//     const questionIndex = Math.floor(availableQuestions.length * Math.random());
-//     currentQuestion = availableQuestions[questionIndex];
-//     question.innerHTML = currentQuestion.question;
-
-//     //Add answers to the challenge.html page
-//     answers.forEach(answer => {
-//         const number = answer.dataset.number;
-//         answer.innerHTML = currentQuestion['option' + number]
-//     });
-
-//     //remove used question from available questions
-//     availableQuestions.splice(questionIndex, 1);
-// };
+    //remove used question from available questions
+    availableQuestions.splice(questionIndex, 1);
+};
 
 // /* Add eventlistener to which answer is given */
 // answers.forEach(option => {
