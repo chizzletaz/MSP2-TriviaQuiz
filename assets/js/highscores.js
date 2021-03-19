934ba062dec7f9561aad726535a687227be532e7
chizzletaz <epitax@hotmail.com>
2021-03-19 12:31:59 +0000

    Layout: add circle around rank on highscores.html.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -4,6 +4,6 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];  //get 
 
 // Add highscores to highscore.html. Credit: James Q Quick: https://www.youtube.com/watch?v=jfOv18lCMmw
 highScoresTable.innerHTML = highScores.map((score, index) => {
-    return `<tr class="high-score"><td class="tac">${index + 1} </td><td class="tal">${score.name}</td><td class="tar">${score.score}</td></tr>`;
+    return `<tr class="high-score"><td class="tac"><span class="rank">${index + 1}</span> </td><td class="tal">${score.name}</td><td class="tar">${score.score}</td></tr>`;
 })
 .join("");
