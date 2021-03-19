b37015477f280ba4dbf63ca94040d033d9af280c
chizzletaz <epitax@hotmail.com>
2021-03-19 12:04:24 +0000

    Add numbering to highscores.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -3,7 +3,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];  //get 
 
 
 // Add highscores to highscore.html. Credit: James Q Quick: https://www.youtube.com/watch?v=jfOv18lCMmw
-highScoresTable.innerHTML = highScores.map(score => {
-    return `<tr class="high-score"><td class="tal">${score.name}</td> <td class="tac"> - </td> <td class="tar">${score.score}</td></tr>`;
+highScoresTable.innerHTML = highScores.map((score, index) => {
+    return `<tr class="high-score"><td class="tac">${index + 1} </td><td class="tal">${score.name}</td><td class="tar">${score.score}</td></tr>`;
 })
 .join("");
